2a9ed38d84b4a355c74ef2ca602d8e67152eb635
Mengyang Liu <myliu@seu.edu.cn>
2022-07-22 15:16:52 +0800

    :construction: Support spawn and create hashmap-based database of redis server

--- code/my-redis/src/main.rs
+++ code/my-redis/src/main.rs
@@ -9,20 +9,44 @@ async fn main() {
     loop {
         // Create socket when connecting
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+
+        //Spawn a new task for each inbound socket.
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
 }
 
 async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    // Create hashmap based database to store values
+    let mut db = HashMap::new();
+
     // Create connection with the socket
     let mut connection = Connection::new(socket);
 
     // Accepet frame from connection
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                // Store the value as `Vec<u8>`
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    // Convert `&Vec<u8>` to `Bytes`
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
 
-        // Respond with an error
-        let response = Frame::Error("unimplemented".to_string());
+        // Response to the client
         connection.write_frame(&response).await.unwrap();
     }
 }
