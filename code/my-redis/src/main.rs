3f9c7205082ce400bae23977716a88c3153cab9f
Mengyang Liu <myliu@seu.edu.cn>
2022-07-22 15:35:25 +0800

    :construction: Support shared database of redis server

--- code/my-redis/src/main.rs
+++ code/my-redis/src/main.rs
@@ -1,28 +1,36 @@
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     // Bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // Create socket when connecting
         let (socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
+
+        println!("Accepted");
 
         //Spawn a new task for each inbound socket.
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    // Create hashmap based database to store values
-    let mut db = HashMap::new();
 
     // Create connection with the socket
     let mut connection = Connection::new(socket);
@@ -31,11 +39,13 @@ async fn process(socket: TcpStream) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
+                let mut db = db.lock().unwrap();
                 // Store the value as `Vec<u8>`
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     // Convert `&Vec<u8>` to `Bytes`
                     Frame::Bulk(value.clone().into())
