a98c72ca37fdcc10a30f65a6c8c5b52259469fec
Mengyang Liu <myliu@seu.edu.cn>
2022-08-23 16:54:24 +0800

    :construction: Sever now can validate the request and selectively responding

--- code/my-web-server/src/main.rs
+++ code/my-web-server/src/main.rs
@@ -20,20 +20,30 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     // create reader to read stream
     let buf_reader = BufReader::new(&mut stream);
-    // transform stream into a vector constituted by many string lines
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    // just get request line
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    
+    if request_line == "GET / HTTP/1.1" {
+        // GET / request
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+        
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        // some other request
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
 
-    let response = 
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
 
-    stream.write_all(response.as_bytes()).unwrap();
-
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
