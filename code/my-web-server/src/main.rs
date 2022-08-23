287f21fed514fd2457e20a40cfc2e57b7828af97
Mengyang Liu <myliu@seu.edu.cn>
2022-08-23 16:31:22 +0800

    :construction: Sever now can read the request

--- code/my-web-server/src/main.rs
+++ code/my-web-server/src/main.rs
@@ -1,11 +1,32 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
+    // create listener
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
+    // listen incoming requests
     for stream in listener.incoming() {
+        // receive message as stream
         let stream = stream.unwrap();
-
-        println!("Connection established!");
+        
+        // handle stream
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    // create reader to read stream
+    let buf_reader = BufReader::new(&mut stream);
+    // transform stream into a vector constituted by many string lines
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    // show messages inside the request
+    println!("Request: {:#?}", http_request);
+
 }
