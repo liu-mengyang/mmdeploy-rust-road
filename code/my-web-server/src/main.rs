63e8029887276dc9052f4a57adc3ce1ad8de900e
Mengyang Liu <myliu@seu.edu.cn>
2022-08-23 16:57:12 +0800

    :construction: Refactor main.rs to reduce repetition

--- code/my-web-server/src/main.rs
+++ code/my-web-server/src/main.rs
@@ -23,27 +23,18 @@ fn handle_connection(mut stream: TcpStream) {
     // just get request line
     let request_line = buf_reader.lines().next().unwrap().unwrap();
     
-    if request_line == "GET / HTTP/1.1" {
-        // GET / request
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
-
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-        
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
     } else {
-        // some other request
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let length = contents.len();
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+    let response = format!(
+        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
