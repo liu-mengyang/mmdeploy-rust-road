550927b185f682bdd68ae05d1240ee1c8d9ca612
Mengyang Liu <myliu@seu.edu.cn>
2022-08-24 16:56:17 +0800

    :construct: Send requests to threads via channels

--- code/my-web-server/src/lib.rs
+++ code/my-web-server/src/lib.rs
@@ -1,20 +1,31 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        // Arc let multiple workers own the receiver,
+        // and Mutex will ensure that only one worker gets a job at a time.
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
             // create some workers and store them in the vector
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -30,8 +41,10 @@ struct Worker {
 }
 
 impl Worker {
-    pub fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
         Worker { id, thread }
     }
 }
