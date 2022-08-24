e21c478bc03f7fed40eada9d600c6637dce6df64
Mengyang Liu <myliu@seu.edu.cn>
2022-08-24 16:44:27 +0800

    :construct: Use worker in thread pool

--- code/my-web-server/src/lib.rs
+++ code/my-web-server/src/lib.rs
@@ -1,24 +1,37 @@
 use std::thread;
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
-            // create some threads and store them in the vector
+        for id in 0..size {
+            // create some workers and store them in the vector
+            workers.push(Worker::new(id));
         }
-        ThreadPool { threads }
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+        Worker { id, thread }
     }
 }
