20050383ec583c17fb7ee3ec418cf41f74e21504
Mengyang Liu <myliu@seu.edu.cn>
2022-08-25 14:17:46 +0800

    :construction: Implement the Drop trait on ThreadPool

--- code/my-web-server/src/lib.rs
+++ code/my-web-server/src/lib.rs
@@ -40,9 +40,21 @@ impl ThreadPool {
     }
 }
 
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
@@ -54,6 +66,9 @@ impl Worker {
             
             job();
         });
-        Worker { id, thread }
+        Worker { 
+            id,
+            thread: Some(thread),
+        }
     }
 }
